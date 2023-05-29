fa65b5bf82214f770f53403d6a61a0226363c8ba
Lavasku <mantequerad@uninorte.edu.co>
2023-05-29 14:24:07 -0500

    created index.js

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import userRouter from './user/user.router';
+import productRouter from './product/product.router';
+import orderRouter from './order/order.router';
+import mongoose from 'mongoose';
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+
+app.use('/user', userRouter);
+app.use('/product', productRouter);
+app.use('/order', orderRouter);
+
+mongoose
+  .connect('mongodb+srv://cluster0.jnp7asc.mongodb.net/',{
+    dbName: 'delivery',
+    user: 'test',
+    pass:'testeo',
+  })
+  .then(()=> console.log('database connected'))
+  .catch((error) => console.log(error))
+
+
+
+try {
+  app.listen(port);
+  console.log('server running on port ' + port);
+} catch (error) {
+  console.log(error);
+}
